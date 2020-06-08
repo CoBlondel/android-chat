412d091afadb1cceb725b5b6c0e738bddbfb4081
Corentin <coblondel@gmail.com>
2020-06-08 12:04:08 +0200

    - transform preferences activity to fragment

---
+++ app/src/main/java/com/example/final_ig2i/PreferencesFragment.java
@@ -0,0 +1,12 @@
+package com.example.final_ig2i;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class PreferencesFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+}
